d5f45dd02b3988fc86455a9aca20f925aa82d76f
RillSvai <kirilkvas07@gmail.com>
2023-11-14 21:21:47 +0200

    Configuration in start and working on MainView

--- CryptocurrencyInformationApp/App.xaml.cs
+++ CryptocurrencyInformationApp/App.xaml.cs
@@ -1,17 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CryptocurrencyInformationApp.ViewModels.Main;
+using CryptocurrencyInformationApp.Views;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using System.Windows;
 
 namespace CryptocurrencyInformationApp
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
+
     public partial class App : Application
     {
+        public static IHost? AppHost {  get; set; }
+
+        public App()
+        {
+            AppHost = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddSingleton<MainView>();
+                    services.AddSingleton<MainViewModel>();
+                }).Build();
+        }
+        protected override async void OnStartup(StartupEventArgs e)
+        {
+            await AppHost!.StartAsync();
+            MainView mainView = AppHost!.Services.GetRequiredService<MainView>();
+            mainView.Show();
+            base.OnStartup(e);
+        }
+
+        protected override async void OnExit(ExitEventArgs e)
+        {
+            await AppHost!.StopAsync();
+            base.OnExit(e);
+        }
     }
 }
